fdd7cc86276b74bc1adba2d2c21e5a4aad4777d7
plumbing <plumbing@plumbing.water>
2019-09-04 01:59:38 -0500

    bones

--- iosci.jl
+++ iosci.jl
@@ -4,6 +4,7 @@ using Blink
 using OmniSci
 using Interact
 
+### Custom widgets made of textboxes and activated from the Connect button
 di = OrderedDict(
     :localhost => textbox("localhost"),
     :port => textbox("port"),
@@ -12,23 +13,41 @@ di = OrderedDict(
     :db => textbox("Database")
 )
 wi = Widget{:connector}(di)
-
 connbtn = button("Connect")
 
 map(connbtn -> connect(di))
-#conn = connect(wi[:localhost], wi[:port], wi[:user], wi[:password], wi[:db])
 
 
+# Create and Load Table Widgets #
 
-tab = radiobuttons(Dict("Create Table" => "ct", "Load Table" => "lt"))
+#tab = radiobuttons(Dict("Create Table" => "ct", "Load Table" => "lt"))
+
+# Create Table Widget
+ctable = OrderedDict(
+    :tableName => textbox("Table Name"),
+    :addTable =>  button("Create Data Table")
+)
+ct = Widget{:connector}(ctable)
+
+# Load Table Widget
+ltable = OrderedDict(
+    :fp => filepicker(label="Data File"; multiple=false, accept=".csv"),
+    :tableName => textbox("Table Name"),
+    :loadTable =>  button("Load Data Table")
+)
+lt = Widget{:connector}(ltable)
 
 
+#Tabs for the Data Table page Create/Load
+clt = OrderedDict(
+    "Create Table" => vbox(ct),
+    "Load Table" => vbox(lt)
+)
 
-
-
+#Tabs for each sheet in the app
 uu = OrderedDict(
     "Connect" => hbox(wi, connbtn),
-    "Data" => vbox(tab),
+    "Data" => tabulator(clt),
     "Custom SQL" => vbox(println("SQL"))
 )
 
